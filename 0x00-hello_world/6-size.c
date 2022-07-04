d4fc091f7f99c2c257de774480ca8c196d894769
nebiyou10 <walkingdead1e@gmail.com>
2022-07-04 13:44:18 -0700

    lost man

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,11 @@
 */
 int main(void)
 {
+int i;
+long int l;
+long long int ll;
+float d;
+char c;
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(l));
