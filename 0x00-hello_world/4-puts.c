37cadf50c1a97115d9d82a78b82e71cf33a385f5
nebiyou10 <106772609+nebiyou10@users.noreply.github.com>
2022-07-05 03:47:52 +0300

    Add files via upload

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry point.
+*Description: Prints the description specified
+*Return: always zero(success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
